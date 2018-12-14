96073a097a034c46bcbcf7df01df718e0ea8f7ad
Fabian Carlström <git@inrick.se>
2018-12-14 15:48:52 +0100

    day14

---
+++ day14/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	input := 110201
+	digits := Digits(input)
+	D := len(digits)
+
+	board := []int{3, 7}
+	c1, c2 := 0, 1 // Elves
+
+	firstAppears := -1
+
+Outer:
+	for len(board) < input+10 || firstAppears == -1 {
+		newRecipes := Digits(board[c1] + board[c2])
+		board = append(board, newRecipes...)
+		N := len(board)
+		c1 = (c1 + 1 + board[c1]) % N
+		c2 = (c2 + 1 + board[c2]) % N
+
+		// Every round adds at most two digits. See if there's a match.
+		for i := N - D - 2; firstAppears == -1 && 0 <= i && i <= N-D; i++ {
+			j := i
+			for ; j < N && board[j] != digits[0]; j++ {
+			}
+			if j > N-D {
+				// Not enough digits left for a match
+				continue Outer
+			}
+			k := 0
+			for ; k < D && board[j+k] == digits[k]; k++ {
+			}
+			if k == D {
+				firstAppears = j
+			}
+		}
+	}
+
+	fmt.Print("a) ")
+	for _, d := range board[input : input+10] {
+		fmt.Print(d)
+	}
+	fmt.Println()
+	fmt.Printf("b) %d\n", firstAppears)
+}
+
+func Digits(n int) []int {
+	if n == 0 {
+		return []int{0}
+	}
+	N := 1 + int(math.Log10(float64(n)))
+	digits := make([]int, N)
+	for i := range digits {
+		a := int(math.Pow(10, float64(N-i)))
+		b := int(math.Pow(10, float64(N-i-1)))
+		digits[i] = (n % a) / b
+	}
+	return digits
+}
